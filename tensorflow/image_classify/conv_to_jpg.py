faf493c6315bd520fb750f9c4689bc52903f24b3
gjz22cn <gjz22cn@hotmail.com>
2019-03-16 13:53:41 +0800

    Develop | add for image_classify

---
+++ tensorflow/image_classify/conv_to_jpg.py
@@ -0,0 +1,17 @@
+import os
+import cv2
+import sys
+import numpy as np
+
+path = "./data/pre_image/"
+print(path)
+
+for filename in os.listdir(path):
+    if os.path.splitext(filename)[1] == '.png':
+        # print(filename)
+        img = cv2.imread(path + filename)
+        print(filename.replace(".png", ".jpg"))
+        newfilename = filename.replace(".png", ".jpg")
+        # cv2.imshow("Image",img)
+        # cv2.waitKey(0)
+        cv2.imwrite(path + newfilename, img)
